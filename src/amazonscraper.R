1617c6f9859a9587b89f647805b3171000a7e577
Kirill Konovalov <auddax@gmail.com>
2019-02-11 19:42:08 +0300

    Complete amazon scraper function

--- src/amazonscraper.R
+++ src/amazonscraper.R
@@ -1,52 +1,52 @@
 #Parse Amazon html pages for data
-amazon_scraper <- function(doc, delay = 0){
+amzn_scr <- function(data){
   
   require(rvest)
   require(stringr)
   
-  sec = 0
-  if(delay < 0) warning("delay was less than 0: set to 0")
-  if(delay > 0) sec = max(0, delay + runif(1, -1, 1))
-  
-  
-  title <- doc %>%
+  title <- data %>%
     html_nodes("#cm_cr-review_list .a-color-base") %>%
     html_text()
   
-  author <- doc %>%
+  author <- data %>%
     html_nodes("#cm_cr-review_list .a-profile-name") %>%
     html_text()
-  if(length(author) == 0) author <- NA
   
-  date <- doc %>%
+  
+  date <- data %>%
     html_nodes("#cm_cr-review_list .review-date") %>%
     html_text() %>% 
-    gsub(".*on ", "", .)
+    gsub(".*on ", "", .) %>%
+    as.Date(format = "%B %d, %Y")
   
-  ver.purchase <- doc%>%
+  ver.purchase <- data%>%
     html_nodes(".review-data.a-spacing-mini") %>%
     html_text() %>%
     grepl("Verified Purchase", .) %>%
     as.numeric()
   
-  format <- doc %>% 
+  format <- data %>% 
     html_nodes(".review-data.a-spacing-mini") %>% 
     html_text() %>%
-    gsub("Color: |\\|.*|Verified.*", "", .)
-  if(length(format) == 0) format <- NA
+    gsub("Color: |\\|.*|Verified.*", "", .) %>% 
+    gsub("Format: ", "", .)
   
-  stars <- doc %>%
+  stars <- data %>%
     html_nodes("#cm_cr-review_list  .review-rating") %>%
     html_text() %>%
     str_extract("\\d") %>%
     as.numeric()
   
-  comments <- doc %>%
+  comments <- data %>%
     html_nodes("#cm_cr-review_list .review-text") %>%
     html_text() 
   
   df <- data.frame(title, author, date, ver.purchase, format, stars, comments, stringsAsFactors = F)
   
-  return(df)
+  if (nrow(df) != 0) {
+    return(df)
+  } else {
+    return(NULL)
+  }
 }
 
--- src/draft.R
+++ src/draft.R
@@ -2,13 +2,17 @@ require(xml2)
 
 source('~/Programming/AmazonReviews/src/amazonscraper.R')
 
-pages <- 2
+pages <- 150
 id <- "B002RI99IM"
 reviews_all <- NULL
 
-for(page_num in 1:pages){
+for(page_num in 100:pages){
   url <- paste0("http://www.amazon.com/product-reviews/", id,"/?pageNumber=", page_num)
-  doc <- read_html(url)
-  reviews <- amazon_scraper(doc, delay = 2)
-  reviews_all <- rbind(reviews_all, reviews)
+  raw_data <- read_html(url)
+  reviews <- amzn_scr(raw_data)
+  if (!is.null(reviews)) {
+    reviews_all <- rbind(reviews_all, reviews)
+  } else {
+    break
+  }
 }
